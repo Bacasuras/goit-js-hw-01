bdc7507e87db3a54d1c84900f1375e13dad412ae
Bacasuras <bacasurasbaca@gmail.com>
2023-11-25 13:15:30 +0200

    hw-1-2

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,9 @@ function getElementWidth(content, padding, border) {
     const contentWidth = parseFloat(content);
   const paddingWidth = parseFloat(padding);
   const borderWidth = parseFloat(border);
-  const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-  console.log(totalWidth); 
+    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+    return totalWidth;
 }
-getElementWidth("50px", "8px", "4px");
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
